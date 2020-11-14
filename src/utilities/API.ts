6bb03fc28617e72b36686855caebf4199a1a96cf
Sarthak <sarthakbh321@gmail.com>
2020-11-15 00:48:10 +0530

    defined api call types

--- src/utilities/API.ts
+++ src/utilities/API.ts
@@ -6,6 +6,17 @@ export enum Difficulty {
 	HARD = "hard",
 }
 
+export type Question = {
+	category: string;
+	correct_answer: string;
+	difficulty: string;
+	incorrect_answers: string[];
+	question: string;
+	type: string;
+};
+
+export type QuestionState = Question & { answers: string[] };
+
 export const fetchQuestions = async (
 	amount: number,
 	difficulty: Difficulty
@@ -15,8 +26,18 @@ export const fetchQuestions = async (
 	try {
 		await Axios.get(url).then((res) => {
 			console.log(res.data);
+			return res.data.results.map((question: Question) => ({
+				...question,
+				answers: shuffleArray([
+					...question.incorrect_answers,
+					question.correct_answer,
+				]),
+			}));
 		});
 	} catch (error) {
 		console.log(error);
 	}
 };
+
+const shuffleArray = (array: any[]) =>
+	[...array].sort(() => Math.random() - 0.5);
